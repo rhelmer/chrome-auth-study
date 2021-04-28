2a7cd9bff67425d9644c045c6254ea45abdca1ac
Robert Helmer <rhelmer@rhelmer.org>
2021-04-27 23:02:16 -0700

    open control panel by default

--- public/options.js
+++ public/options.js
@@ -1,6 +1,9 @@
 const port = chrome.runtime.connect();
 port.onMessage.addListener(message => {
     console.debug(message);
+    document.getElementById("error").textContent = "";
+    document.getElementById("result").textContent = "";
+
     if ("error" in message) {
         document.getElementById("error").textContent = message.error;
     } else if ("result" in message) {
@@ -12,4 +15,9 @@ document.getElementById("test-form").onsubmit = () => {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     port.postMessage({ email, password });
+    document.getElementById("dialog").open = true;
 }
+
+document.getElementById("google-sign-in").addEventListener("click", () => {
+    console.debug("click!");
+});
